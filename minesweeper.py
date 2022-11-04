f62d5a96617746e2f26850dc7bfdfe08f82388f1
Jalco28 <75587027+Jalco28@users.noreply.github.com>
2022-11-04 18:55:01 +0000

    Added some tile class methods

--- minesweeper.py
+++ minesweeper.py
@@ -7,17 +7,18 @@ BOMB_IMAGE = tk.PhotoImage(file="bomb_icon.png")
 FLAG_IMAGE = tk.PhotoImage(file="flag_icon.png")
 BLANK_IMAGE = tk.PhotoImage(file="blank_tile.png")
 TILE_SIZE = 30
+flags_to_place = 40
 info_frame = tk.Frame(height=30)
 grid_frame = tk.Frame()
 
 class Tile(tk.Button):
     def __init__(self, parent, row, column):
-        super().__init__(parent, width=TILE_SIZE, height=TILE_SIZE, image = BLANK_IMAGE)
+        super().__init__(parent, width=TILE_SIZE, height=TILE_SIZE, image = BLANK_IMAGE, bg = 'gray',activebackground='gray', command=self.toggle_flag)
         self.neighbours = []      #Stores number of adjacent bombs
-        self.flagged = False        #If flagged by player
+        self._flagged = False        #If flagged by player
         self.bomb = None            #If tile is a bomb
-        self.hidden = True          #If tile conented is hidden
-        self.highlighted = False    #If highlighted by chording check
+        self.hidden = True          #If tile conentents is hidden
+        self._highlighted = False    #If highlighted by chording check
         self.row = row
         self.column = column
 
@@ -37,6 +38,24 @@ class Tile(tk.Button):
                 continue
             self.neighbours.append([new_row, new_column])
 
+    def set_image(self, image):
+            self.config(image = image)
+
+    def toggle_highlight(self):
+        self._highlighted = False if self._highlighted else True    #Togggle between True and False
+        if self._highlighted:
+            self.config(bg='#b8b8b8', activebackground='#b8b8b8')
+        else:
+            self.config(bg = 'gray', activebackground='gray')
+
+    def toggle_flag(self):
+        self._flagged = False if self._flagged else True    #Togggle between True and False
+        if self._flagged:
+            self.set_image(FLAG_IMAGE)
+            flags_to_place -= 1
+        else:
+            self.set_image(BLANK_IMAGE)
+            flags_to_place += 1
 
 
 tile_map = []
@@ -55,7 +74,8 @@ for row in tile_map:
     for tile in row:
         tile.set_neighbours()
 
-print(tile_map[0][0].neighbours)
+flag_image_label = tk.Label(info_frame, image = FLAG_IMAGE)
+flag_image_label.pack(side='left')
 
 info_frame.pack()
 grid_frame.pack()
